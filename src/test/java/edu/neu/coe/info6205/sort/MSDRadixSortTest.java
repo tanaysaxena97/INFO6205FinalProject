edb4f9374a82477e01249ccdcf17ce82fadf9cf0
tanaysaxena97 <tanaysaxena97@gmail.com>
2021-11-29 01:52:30 -0500

    added new test

--- src/test/java/edu/neu/coe/info6205/sort/MSDRadixSortTest.java
+++ src/test/java/edu/neu/coe/info6205/sort/MSDRadixSortTest.java
@@ -1,37 +1,41 @@
 package edu.neu.coe.info6205.sort;
 
+import edu.neu.coe.info6205.util.FileUtil;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.file.Paths;
+import java.util.*;
 
 public class MSDRadixSortTest {
     @Test
-    public void sortTest() {
+    public void sortTest1() {
+        // test the sorted order
         List<String> input1 = new ArrayList<>(), input2;
-        input1.add("uiytu");
-        input1.add("zuwkak");
-        input1.add("yhtzybe");
-        input1.add("gxprcuhjy");
-        input1.add("rlhtefjsmq");
-        input1.add("iwpwc");
-        input1.add("pogamb");
-        input1.add("lopx");
-        input1.add("yabhipor");
-        input1.add("ysbqxmist");
-        input1.add("ioonvndb");
-        input1.add("senhjgqlc");
-        input1.add("awoys");
-        input1.add("bvzcbq");
-        input1.add("bfzyq");
-        input1.add("cnkpnahkhg");
-        input1.add("xswxk");
+        FileUtil fu = new FileUtil();
+        input1 = fu.readFile(Paths.get(System.getProperty("user.dir"),"src", "main", "resources", "englishStrings.txt").toString());
         input2 = new ArrayList<>(input1);
         input1 = (new MSDRadixSort()).sort(input1);
         Collections.sort(input2);
         for (int i = 0; i < input1.size(); i++) {
             assertEquals(input2.get(i), input1.get(i));
+        }
+    }
+
+    @Test
+    public void sortTest2() {
+        // test if none of the keys has been altered or is missing
+        FileUtil fu = new FileUtil();
+        List<String > xs = fu.readFile(Paths.get(System.getProperty("user.dir"),"src", "main", "resources", "shuffledChinese.txt").toString());
+        Set<String> s1 = new HashSet<>();
+        Set<String> s2 = new HashSet<>();
+        xs.stream().forEach(x -> s1.add(x));
+        xs = (new MSDRadixSort()).sort(xs);
+        xs.stream().forEach(x -> s2.add(x));
+        assertEquals(s1.size(), s2.size());
+        for (String key: s1) {
+            assertTrue(s2.contains(key));
         }
     }
 }
